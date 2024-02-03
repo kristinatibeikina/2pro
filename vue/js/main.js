adfc3c41174b5f8305ca79b501733507f65e21ac
Kristina <kristina.tibeykina04@mail.ru>
2024-02-03 17:27:30 +0700

    Возможность редактирование карточки

--- vue/js/main.js
+++ vue/js/main.js
@@ -34,6 +34,7 @@ new Vue({
                     redact: '',
                     completedItems: 0,
                     text: this.cardText,
+                    editing: false,
 
                 };
                 this.column1.push(newCard);
@@ -68,7 +69,9 @@ new Vue({
             this.column4.push(card);
         },
         back(card){
-            this.complaint = true
+                card.completed = true
+
+
             if (card.redact!==''){
                 this.column3.splice(this.column3.indexOf(card),1)
                 this.column2.push(card);
@@ -76,68 +79,25 @@ new Vue({
 
 
         },
-        redactCard(text){
+        redactCard(text, card){
             this.c=true
-            this.complaint = false
+            card.completed = false
             this.redact=text
             this.saveCards();
         },
-
-       /* updateCardStatus() {
-            this.column1.forEach(card => {
-                const completedItems = card.items.filter(item => item.completed).length;
-                const completionPercentage = (completedItems / card.items.length) * 100;
-
-                if(this.column2.length>4 && completionPercentage >= 50) {
-                    this.isDisabled = !this.isDisabled
-                }
-                else{
-                    this.isDisabled = false
-                }
-
-                if (completionPercentage >= 50) {
-                    this.column2.push(card);
-                    this.column1 = this.column1.filter(c => c.id !== card.id);
-
-                }
-
-            });
-
-            this.column2.forEach(card => {
-                const completedItems = card.items.filter(item => item.completed).length;
-                const completionPercentage = (completedItems / card.items.length) * 100;
-
-                if (completionPercentage < 50) {
-                    this.column1.push(card);
-                    this.column2 = this.column2.filter(c => c.id !== card.id);
-
-                }
-                if (completionPercentage === 100) {
-                    card.completedItems = completedItems;
-                    this.column3.push(card);
-                    this.column2 = this.column2.filter(c => c.id !== card.id);
-                }
-            });
-
-            this.saveCards();
-        },*/
-        /*loadCards() {
-            const savedCards = JSON.parse(localStorage.getItem('cards'));
-
-            if (savedCards) {
-                this.column1 = savedCards.column1 || [];
-                this.column2 = savedCards.column2 || [];
-                this.column3 = savedCards.column3 || [];
-            }
-        },*/
         removeMask(index, type){
             this.column1.splice(index,1)
 
         },
-        editCard() {
-            this.editing = true;
+        editCard(card) {
+            card.editing = true;
 
         },
+        redactCard(card,text){
+            card.editing = false
+            card.text=text
+            this.saveCards()
+        }
 
     },
 
